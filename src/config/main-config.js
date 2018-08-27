75cbec4bcc1f1b2bc7300fd3f23abd0cd369889b
damianrivas <drivas1993@gmail.com>
2018-08-26 21:03:12 -0700

    Configure routes, controllers and ejs view engine

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
+const path = require("path");
+const viewFolder = path.join(__dirname, "..", "views");
 
 module.exports = {
-  init() {}
+  init(app, express) {
+    app.set("views", viewFolder);
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+  }
 };
