b1fcf898397c1a79121f6dd852e61ce933249154
Paul Brower <pabrower@icloud.com>
2018-10-07 19:16:47 -0700

    modified config and test for static view

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,10 +4,10 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to CoolReddit' in the body of the response", done => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(body).toContain("Welcome to CoolReddit");
+        expect(body).toContain("Welcome to Bloccit");
         done();
       });
     });
