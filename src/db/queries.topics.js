6c03e1a6bfa0dfffe1d5f4e23e9cd5eba7f75180
Paul Brower <pabrower@icloud.com>
2018-10-20 19:36:42 -0700

    completed assignment - implemented flair feature

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,5 +1,6 @@
 const Topic = require("./models").Topic;
 const Post = require("./models").Post;
+const Flair = require("./models").Flair;
 
 module.exports = {
   getAllTopics(callback) {
@@ -30,6 +31,9 @@ module.exports = {
       include: [{
         model: Post,
         as: "posts"
+      }, 
+      { model: Flair,
+          as: "flairs"
       }]
     })
       .then(topic => {
