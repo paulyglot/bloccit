fe3ae8ccb6ae0e2c91eb8d515b41b713b7b50d94
Paul Brower <pabrower@icloud.com>
2018-10-30 21:26:13 -0700

    modified post model

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -1,39 +1,49 @@
-"use strict";
- module.exports = (sequelize, DataTypes) => {
-   var Post = sequelize.define("Post", {
-     title: {
-       type: DataTypes.STRING,
-       allowNull: false
-     },
-     body: {
-       type: DataTypes.STRING,
-       allowNull: false
-     },
-     topicId: {
-       type: DataTypes.INTEGER,
-       allowNull: false
-     },
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Post = sequelize.define('Post', {
+
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    topicId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
     userId: {
       type: DataTypes.INTEGER,
       allowNull: false
-    } 
-   }, {});
+    }
+  }, {});
+  Post.associate = function(models) {
+    // associations can be defined here
 
-   Post.associate = function(models) {
-     Post.belongsTo(models.Topic, {
-       foreignKey: "topicId",
-       onDelete: "CASCADE"
-     });    
+    Post.belongsTo(models.Topics, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE"
+    });
 
     Post.belongsTo(models.User, {
-    foreignKey: "userId",
-    onDelete: "CASCADE"
+      foreignKey: "userId",
+      onDelete: "CASCADE"
     });
 
     Post.hasMany(models.Comment, {
       foreignKey: "postId",
       as: "comments"
-    }); 
+    });
+
+    Post.hasMany(models.Vote, {
+      foreignKey: "postId",
+      as: "votes"
+    });
 
     Post.hasMany(models.Favorite, {
       foreignKey: "postId",
@@ -44,27 +54,30 @@
       return models.Favorite.create({
         userId: post.userId,
         postId: post.id
+     });
+   });
+  };
+ Post.prototype.isOwner = function() {
+   return this.userId === this.foreignKey;
+  };
+  Post.prototype.getPoints = function() {
+
+    // #1
+    if (this.votes.length === 0) return 0
+
+    // #2
+    return this.votes
+      .map((v) => {
+        return v.value
+      })
+      .reduce((prev, next) => {
+        return prev + next
       });
+  };
+  Post.prototype.getFavoriteFor = function(userId) {
+    return this.favorites.find((favorite) => {
+      return favorite.userId == userId
     });
-
-    Post.hasMany(models.Vote, {
-      foreignKey: "postId",
-      as: "votes"
-    });
-
-    Post.prototype.getPoints = function(){
-
-      // #1
-          if(this.votes.length === 0) return 0
-     
-      // #2
-          return this.votes
-            .map((v) => { return v.value })
-            .reduce((prev, next) => { return prev + next });
-        };
-    Post.prototype.getFavoriteFor = function(userId){
-      return this.favorites.find((favorite) => { return favorite.userId == userId });
-    };
-   };
-   return Post;
- };
+  };
+  return Post;
+};
